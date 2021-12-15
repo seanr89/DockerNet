6e4c172f7670233685119d5c6ef7c0925ed67a41
Sean <srafferty89@gmail.com>
2021-12-15 20:40:27 +0000

    program and launc hitems

--- src/DockerMinAPI/Program.cs
+++ src/DockerMinAPI/Program.cs
@@ -18,8 +18,10 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-app.UseAuthorization();
+// app.UseAuthorization();
 
 app.MapControllers();
+
+app.MapGet("/", () => "Hello World!");
 
 app.Run();
